b73798a9625732a093024e0d5096fee185533790
useframi1e <yousseframi@aucegypt.edu>
2023-03-13 00:44:12 +0200

    added a function to print the truth table, still working on getting the minterms

--- main.cpp
+++ main.cpp
@@ -1,53 +1,133 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <cmath>
 using namespace std;
 
-vector<string> getMinterms(string input)
+set<char> validChars;
+
+bool isValid(string input, set<char> validChars)
+{
+    int i = 0;
+    bool valid = false;
+    while (i < input.length() && ((input[i] >= 65 && input[i] <= 90) || (input[i] >= 97 && input[i] <= 122) || validChars.find(input[i]) != validChars.end()))
+    {
+        i++;
+    }
+    if (i == input.length())
+        valid = true;
+    return valid;
+}
+
+//still not finished
+vector<int> getMinterms(string input,set<char> variables)
 {
     string minterm = "";
-    vector<string> minterms;
-
+    vector<int> minterms;
+    int bitValue;
     for (int i = 0; i < input.length(); i++)
     {
-        if (input[i] == '+' || input[i] == ' ')
+        while (i<input.length() && input[i] != '+' && input[i] != ' ')
         {
-            if (minterm != "")
-                minterms.push_back(minterm);
-            minterm = "";
+            minterm+=input[i];
+            i++;
         }
-        else
+        while (i<input.length() && (input[i] == '+' || input[i] == ' '))
         {
-            minterm += input[i];
+            i++;
         }
+        
+        minterms.push_back(0);
+        for (int j = 0; j < minterm.length(); j++)
+        {
+            if (j+1<minterm.length() && minterm[j+1] == 39)
+            {
+                bitValue = 0;
+            }
+            else if (j+1<minterm.length() && minterm[j+1] != 39)
+            {
+                bitValue = 1;
+            }
+            minterms[minterms.size()-1] += bitValue*pow(2,variables.size()-j-1);
+        }
+        minterm = "";
     }
-    if (minterm != "")
-        minterms.push_back(minterm);
-
+ 
     return minterms;
 }
 
-set<char> getVariables(vector<string> minterms)
+set<char> getVariables(string input)
 {
     set<char> variables;
-    for (int i = 0; i < minterms.size(); i++)
+    for (int i = 0; i < input.length(); i++)
     {
-        for (int j = 0; j < minterms[i].length(); j++)
+        if (validChars.find(input[i]) == validChars.end())
         {
-            if (minterms[i][j] != 39) // 39 = ascii value of apostrophe
-            {
-                variables.insert(minterms[i][j]);
-            }
+            variables.insert(input[i]);
         }
     }
+    
     return variables;
+}
+
+void printTruthTable(vector<int> minterms, set<char> variables)
+{
+    int truthValue = 0;
+    vector<vector<int> > truthTableCombs(pow(2,variables.size()),vector<int>(variables.size()));
+    for (int i = 0; i < variables.size(); i++)
+    {
+        truthValue = 0;
+        int x = 0;
+        for (int j = 0; j < pow(2,variables.size()); j++)
+        {
+            if (x == pow(2,variables.size()-i-1))
+            {
+                truthValue = (truthValue == 0) ? 1:0;
+                x = 0;
+            }
+            truthTableCombs[j][i] = truthValue;
+            x++;
+        }
+    }
+
+    vector<int> output(pow(2,variables.size()));
+    for (int i = 0; i < minterms.size(); i++)
+    {
+        output[minterms[i]] = 1;
+    }
+    
+
+    for (int i = 0; i < pow(2,variables.size()); i++)
+    {
+        for (int j = 0; j < variables.size(); j++)
+        {
+            cout<<truthTableCombs[i][j]<<" ";            
+        }
+        cout<<output[i]<<endl;
+    }
+
+    
 }
 
 int main()
 {
-    string input = "abc' + dbc'+d'e' +";
-    vector<string> minterms = getMinterms(input);
-    set<char> variables = getVariables(minterms);
+    validChars.insert(39);
+    validChars.insert(' ');
+    validChars.insert('+');
+    validChars.insert('&');
+    validChars.insert('!');
+    string input = "abc'";
+
+    if (isValid(input, validChars))
+    {
+        cout << "valid" << endl;
+    }
+    else
+        cout << "invalid" << endl;
+
+    set<char> variables = getVariables(input);
+    vector<int> minterms = getMinterms(input, variables);
+    printTruthTable(minterms, variables);
 
     for (int i = 0; i < minterms.size(); i++)
     {
